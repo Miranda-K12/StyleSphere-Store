fd2c4657259ac7339c484f6152c68e49d07283a9
Miranda Kachlavashvili <mirandayachlavashvili@gmail.com>
2024-10-17 19:15:13 +0400

    update js code

--- script/main.js
+++ script/main.js
@@ -9,3 +9,11 @@ hamburger.addEventListener('click', () => {
 });
 
 
+
+const closeBtns = document.querySelectorAll("#burger-menu a"); 
+closeBtns.forEach(function(link) {
+    link.addEventListener("click", function(e) {
+        burgerMenu.classList.remove("active"); 
+        hamburger.classList.remove("active");  
+    });
+});
