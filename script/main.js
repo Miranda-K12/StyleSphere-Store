ae313e919d3ef858522d449dfd78e8e149a2326a
Miranda Kachlavashvili <mirandayachlavashvili@gmail.com>
2024-10-18 00:20:17 +0400

    add js accordion

--- script/main.js
+++ script/main.js
@@ -17,3 +17,22 @@ closeBtns.forEach(function(link) {
         hamburger.classList.remove("active");  
     });
 });
+
+// FAQ Accordion 
+const box = document.querySelectorAll('.box');
+  box.forEach(function (item) {
+    item.addEventListener('click', function () {
+        const answer = this.querySelector('.answer');
+        const icon = this.querySelector('.icon');
+        if (answer) {
+            answer.classList.toggle('display-answer');
+        }
+        if (icon) {
+            if (icon.src.includes("images/icons/down-arrow.svg")) {
+                icon.src = "images/icons/up-arrow.svg";
+            } else {
+                 icon.src = "images/icons/down-arrow.svg";
+            } 
+            }
+    });
+});
